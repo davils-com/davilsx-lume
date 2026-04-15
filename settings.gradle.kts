53edd4c9555b847016ebe51707011204f7bf265a
Nils Jaekel <nils.jaekel@davils.com>
2026-04-15 18:26:09 +0200

    feat: update settings.gradle.kts for project structure and naming

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-rootProject.name = "lume"
-include("davilsx-lume-ui")
-include("davilsx-lume-icons")
+rootProject.name = "davilsx-lume"
 
-include("davilsx-lume-icons:davilsx-lume-icons-lucide")
+// UI
+include(":davilsx-lume-ui")
+
+// Icons
+include(":davilsx-lume-icons")
+include(":davilsx-lume-icons:davilsx-lume-icons-lucide")
