761af117e1b681abe79a271199005753ab3d749b
Nils Jaekel <nils.jaekel@davils.com>
2026-04-16 20:50:19 +0200

    feat: add icon generator configuration for Lucide resources

--- davilsx-lume-icons/davilsx-lume-icons-lucide/build.gradle.kts
+++ davilsx-lume-icons/davilsx-lume-icons-lucide/build.gradle.kts
@@ -18,6 +18,7 @@ plugins {
     `lume-core`
     `lume-common`
     `lume-compose`
+    `lume-icon-generator`
 }
 
 kotlin {
@@ -38,5 +39,34 @@ compose {
     resources {
         nameOfResClass = "LumeLucideResources"
         packageOfResClass = "com.davils.lume.icons.lucide.resources"
+    }
+}
+
+icon {
+    copyright = """
+        Copyright 2026 Davils
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+    """.trimIndent()
+    nameOfResClass = "LumeLucideResources"
+    packageOfResClass = "com.davils.lume.icons.lucide.resources"
+    inputDirectory = file(layout.projectDirectory.dir("src/commonMain/resources/drawable"))
+    outputDir = (layout.projectDirectory.dir("src/commonMain/kotlin"))
+    packageName = "com.davils.lume.icons.lucide"
+
+    kDoc {
+        enabled = true
+        docUrlPattern = "https://lucide.dev/icons/%{icon}"
+        since = "1.0.0"
     }
 }
