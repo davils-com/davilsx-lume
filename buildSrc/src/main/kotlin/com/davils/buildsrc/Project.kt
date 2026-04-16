62e6fd126056ead860b37ea664f8855c5906292b
Nils Jaekel <nils.jaekel@davils.com>
2026-04-16 08:25:44 +0200

    feat: add Project.kt to define project organization and Android SDK versions

---
+++ buildSrc/src/main/kotlin/com/davils/buildsrc/Project.kt
@@ -0,0 +1,12 @@
+package com.davils.buildsrc
+
+public object Project {
+    public object Organisation {
+        public const val GROUP: String = "com.davils"
+    }
+
+    public object Android {
+        public const val COMPILE_SDK: Int = 36
+        public const val MIN_SDK: Int = 26
+    }
+}
